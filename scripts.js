6a0e80a8531db1bfd7dd1fb3843421362ffa14af
kMose <bennegron62@gmail.com>
2022-08-01 07:50:35 -0400

    Add basic javascript operator functions

---
+++ scripts.js
@@ -0,0 +1,41 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+
+function multiply(x, y){
+    return x*y;
+}
+
+
+function divide(x, y){
+
+    if (x == 0 || y == 0) return "You can't divide by zero, you dingus!";
+    return x / y;
+
+}
+
+
+function operate(x, y, operator){
+
+    switch(operator){
+        case '+':
+            return add(x,y);
+        case '-':
+            return subtract(x,y);
+        case '*':
+            return multiply(x,y);    
+        case '/':
+            return divide(x,y);        
+
+
+
+    }
+
+
+}
+
